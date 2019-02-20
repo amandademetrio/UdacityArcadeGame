6ab87005079d202faa8d3b4af77df2412d15f02f
amandademetrio <amandademetrio@gmail.com>
2019-02-19 21:12:57 -0800

    commit

---
+++ js/app.js
@@ -0,0 +1,123 @@
+// Enemies our player must avoid
+var Enemy = function(position, speed) {
+    this.sprite = 'images/enemy-bug.png';
+    //enemies step size for right and left moving
+    this.jump = 75;
+    this.step = 101;
+    this.x = 0;
+    this.y = position * this.jump;
+    this.speed = speed;
+};
+
+Enemy.prototype.update = function(dt) {
+    //if the enemy has reached the end of the board, reset x position and change speed for next loop
+    if (this.x >= 505) {
+        this.x = 0;
+    }
+    else {
+        //if there's still space to move, move enemy according to enemy's speed
+        this.x += this.step * (dt * this.speed);
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    //sizes of jumps and steps for the player, matching step size of enemy
+    this.jump = 75;
+    this.step = 101;
+    this.y = this.jump * 5;
+    this.x = this.step * 2;
+    this.victory = false;
+}
+
+//checking for a collision or a win
+Player.prototype.update = function(dt) {
+    //check for collision
+    this.checkForCollision();
+    //check for win
+    this.checkForWin();
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(code) {
+    //checking which keyCode the user pressed and moving if boundaries allow it to move
+    switch(code) {
+        case "up":
+            if (this.y >= this.jump) {
+                this.y -= this.jump
+            }
+            break;
+        case "down":
+            if (this.y <= this.jump * 4) {
+                this.y += this.jump
+            }
+            break;
+        case "left":
+            if (this.x >= this.step) {
+                this.x -= this.step
+            }
+            break;
+        case "right":
+            if (this.x <= this.step * 3) {
+                this.x += this.step
+            }
+    }
+}
+
+//checks for collision, resets the user position if it detects one
+Player.prototype.checkForCollision = function() {
+    //looping through the enemies positions
+     for (enemy of allEnemies) {
+        //first checking for a row match, then comparing x positions
+        if (this.y == enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+            //reseting the position, if there was a collision
+            this.resetPosition()
+        }
+    }
+}
+
+Player.prototype.checkForWin = function() {
+    if (this.y == 0) {
+        console.log("setting victory to true inside check for win");
+        this.victory = true;
+    }
+}
+
+Player.prototype.resetPosition = function() {
+    //reassigning self
+    let self = this;
+    //after collision or win, wait half a second and resets the user position
+    window.setTimeout(function() {
+        self.y = self.jump * 5;
+        self.x = self.step * 2;
+    },300)
+}
+
+//creating the player
+let player = new Player();
+
+//creating the enemies
+let enemy1 = new Enemy(1,1);
+let enemy2 = new Enemy(2,1);
+let enemy3 = new Enemy(3,1);
+
+let allEnemies = [enemy1,enemy2,enemy3]
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
